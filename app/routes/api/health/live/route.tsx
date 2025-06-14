3f3e9e5f85146dd62e7320871cd0e91329d20b0e
Galdámez <im@galdoway.dev>
2025-06-14 16:28:50 -0600

    feat: service name added to healtcheck endpoints

--- app/routes/api/health/live/route.tsx
+++ app/routes/api/health/live/route.tsx
@@ -14,6 +14,7 @@ export function meta() {
 export function loader({ }: LoaderFunctionArgs) {
   return {
     status: "UP",
+    service: process.env?.APP_NAME,
     timestamp: new Date().toISOString()
   }
 }
--- app/routes/api/health/ready/route.tsx
+++ app/routes/api/health/ready/route.tsx
@@ -14,6 +14,7 @@ export function loader({ }: LoaderFunctionArgs) {
   return {
     status: "UP",
     timestamp: new Date().toISOString(),
+    service: process.env.APP_NAME,
     checks: {
       database: {
         status: "UP",
