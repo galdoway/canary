507d2776699dcfe79d35f2dd2e00a04be8d317a0
Galdámez <im@galdoway.dev>
2025-06-14 16:38:33 -0600

    feat: button component added

--- app/routes/home/page.tsx
+++ app/routes/home/page.tsx
@@ -1,16 +1,21 @@
+import { Button } from '~/components/ui/button';
 import type { Route } from './+types/page';
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Canary: Mock App" },
+    { name: "description", content: "Welcome to Canary!" },
   ];
 }
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className='min-h-screen flex flex-col gap-2 justify-center items-center'>
+      <div className='text-center'>
+        <h1 >Hello, World!</h1>
+        <p>Canary App</p>
+      </div>
+      <Button>This is a button.</Button>
     </div>
   );
 }
